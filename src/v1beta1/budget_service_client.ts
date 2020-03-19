be6a427dff08ba212e2e1f35ce49c3ff9c582ce6
yoshi-automation <yoshi-automation@google.com>
2020-03-19 04:16:33 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- src/v1beta1/budget_service_client.ts
+++ src/v1beta1/budget_service_client.ts
@@ -414,6 +414,10 @@ export class BudgetServiceClient {
   /**
    * Updates a budget and returns the updated budget.
    *
+   * WARNING: There are some fields exposed on the Google Cloud Console that
+   * aren’t available on this API. Budget fields that are not exposed in
+   * this API will not be changed by this method.
+   *
    * @param {Object} request
    *   The request object that will be sent.
    * @param {google.cloud.billing.budgets.v1beta1.Budget} request.budget
@@ -502,6 +506,11 @@ export class BudgetServiceClient {
   ): void;
   /**
    * Returns a budget.
+   *
+   * WARNING: There are some fields exposed on the Google Cloud Console that
+   * aren’t available on this API. When reading from the API, you will not
+   * see these fields in the return value, though they may have been set
+   * in the Cloud Console.
    *
    * @param {Object} request
    *   The request object that will be sent.
@@ -663,6 +672,11 @@ export class BudgetServiceClient {
   ): void;
   /**
    * Returns a list of budgets for a billing account.
+   *
+   * WARNING: There are some fields exposed on the Google Cloud Console that
+   * aren’t available on this API. When reading from the API, you will not
+   * see these fields in the return value, though they may have been set
+   * in the Cloud Console.
    *
    * @param {Object} request
    *   The request object that will be sent.
